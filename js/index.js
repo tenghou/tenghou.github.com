9007b95399b5a2177a635cd88c724aae727110fd
teng hou <tenghou@hotmail.com>
2017-03-17 14:44:32 +0800

    use baidu statistics
    and add js file

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var mail = "gratubh@ubgznvy.pbz"
+String.prototype.rot13 = function(){
+	return this.replace(/[a-zA-Z]/g, function(c){
+		return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+	});
+};
---
+++ js/statistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?df24606515cc8df1ba8f9a4fb36d0a95";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
